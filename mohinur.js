5f468ec1f564398d4b6ba090a3f6d379108953d6
mmiahASC4 <miahmohinur@gmail.com>
2017-07-11 12:07:45 -0400

    first commit

---
+++ mohinur.js
@@ -0,0 +1,2 @@
+console.log("mohinur is awesocode pokemonVariable.js
+code pokemonVariable.js
---
+++ pokemonVariable.js
@@ -0,0 +1,6 @@
+var charizardHP = 78;
+var charizardNum = 6;
+var charizardType = "Fire and Flying";
+var charizardBestMove = "Flare Blitz";
+var charizardLastEvo = true;
+
