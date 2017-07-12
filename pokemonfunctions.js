78dc95e1bd89ea9b6b8278f17c18dcc0a24672fc
mmiahASC4 <miahmohinur@gmail.com>
2017-07-12 13:25:09 -0400

    my nsg

---
+++ pokemonfunctions.js
@@ -0,0 +1,26 @@
+var alphabet = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+Function randLetter() {
+    //Want to return a random letter
+    var randomLetter = Math.floor(random)()* alphabet.length;
+    return randomLetter; 
+
+}
+Console.log(alphabet[ranLetter];
+
+Function randWord() {
+    // return string of random length of random letters
+    var text = "";
+    var possible = "alphabet";
+
+    for (var i = 0; i < 26; i++)
+    text += possible.charAt(Math.random() * possible.length));
+
+    return text;
+}
+
+console.log(randWord());
+
+
+Function randSentence() {
+
+}
