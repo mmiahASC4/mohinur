194c52fe6a7cb54aaec9dff5b59d140580b43693
mmiahASC4 <miahmohinur@gmail.com>
2017-07-13 12:04:59 -0400

    didnt finish

---
+++ pokemon-roster.js
@@ -0,0 +1,9 @@
+var name = ["Ivysaur", "Raichu", "Machop", "Rapidash", "Mewtwo", "Hounddoom"];
+var data = [
+    ["Ivysaur", "Grass and Poison", "60", "63", "62", "false"] 
+    ["Raichu", "Electric", "60", "55", "90", "false"],
+    ["Machop", "Fighting", "70", "50", "80", "false"],
+    ["Rapidash", "Fire", "65", "70", "100", "false"],
+    ["Mewtwo", "106", "90", "110", "false"],
+    ["Houndoom", "75", "50","90", "false"]
+]
