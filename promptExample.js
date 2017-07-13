20e79c4e02fc2b5ce2603292638b4d16339d041d
mmiahASC4 <miahmohinur@gmail.com>
2017-07-13 11:06:22 -0400

    Super smash

---
+++ promptExample.js
@@ -0,0 +1,7 @@
+var prompt = require('prompt-sync')();
+// 
+// get input from the user. 
+// 
+
+var pokemon = prompt('What pokemon do you want?');
+console.log(pokemon + " is a good choice!");
---
+++ super-smash.js
@@ -0,0 +1,14 @@
+
+var mashLocation = ["1", "2", "3", "4"];
+
+var firstQuestion = ["10", "20"]; 
+
+var secondQuestion = [" Luigi", "Ness",];
+
+var prompt = require('prompt-sync')();
+
+var fight = prompt("How many times will you sytrike? ");
+console.log("You decided to strike " + fight + " times.")
+
+var opponent = prompt("Who will you choose to fight? ");
+console.log("You fought Luigi in location 2 and hit them 10 times. Too bad. Mewtwo wins! ");
